0fc945c042bdf5792533ffe660cd59f5ed0b7a4c
Nnamani Ugochukwu Frederick <royal.ugoh@gmail.com>
2023-09-03 22:04:19 +0100

    Create the nodejs backend to serve index.ejs, process user input and render band.ejs which reveals the band name

--- index.js
+++ index.js
@@ -2,6 +2,32 @@ require('dotenv').config();
 
 const express = require('express');
 const logger = require('morgan');
+const ejs = require('ejs');
 
 const log = console.log;
 const port = process.env.PORT;
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'public/views');
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}));
+app.use(logger('combined'));
+
+app.get('/',  (req, res) => {
+    res.render('index');
+})
+
+app.post('/band/name', (req, res) => {
+    const {cityName, petName} = req.body;
+
+    const message = `Your Band Name Could Be ====  ${cityName} - ${petName}`
+    res.render('band', {message})
+})
+
+
+
+
+app.listen(port, () => {
+    log(`Server is running on port ${port}.`)
+})
